6e19fb804aaa22be54f2b2fea2716f5cc4d11e68
mdata78 <data@student.agh.edu.pl>
2023-05-26 16:58:17 +0200

    Zadanie domowe proba 1

--- src/main/frontend/src/meetings/MeetingsPage.vue
+++ src/main/frontend/src/meetings/MeetingsPage.vue
@@ -14,27 +14,49 @@
 <script>
 import NewMeetingForm from "./NewMeetingForm";
 import MeetingsList from "./MeetingsList";
+import axios from "axios";
 
 export default {
   components: {NewMeetingForm, MeetingsList},
   props: {username: String},
   data() {
     return {
-      meetings: []
+      meetings: [],
+      participants: []
     };
   },
   methods: {
     addNewMeeting(meeting) {
-      this.meetings.push(meeting);
+      this.meetings.push({id: meeting.id, title: meeting.title, description: meeting.description, participants:[]})
+
+        axios
+            .post('/api/meeting',meeting)
+            .then(response => {console.log(response.data.id);console.log(response.data);})
+            .catch(response =>{this.message = "Nie udało się dodać spotkania, spróbuj ponownie"; });
     },
-    addMeetingParticipant(meeting) {
-      meeting.participants.push(this.username);
+    addMeetingParticipant(meeting){
+          meeting.participants.push(this.username);
+          let url ='api/meetings/' + meeting.id + '/participants/';
+      axios
+          .get('api/participatnts/' + this.username)
+          .then(response => axios.post(url, response.data));
+
     },
     removeMeetingParticipant(meeting) {
       meeting.participants.splice(meeting.participants.indexOf(this.username), 1);
-    },
+      let url = 'api/meetings/' + meeting.id + '/participants/' + this.username;
+          axios
+              .get('api/participants/'+this.username)
+              .then(response =>axios.delete(url));
+
+      },
     deleteMeeting(meeting) {
       this.meetings.splice(this.meetings.indexOf(meeting), 1);
+
+      axios
+          .delete('api/meetings/'+ meeting.id)
+          .then(response =>console.log(response.data))
+          .catch(response => {this.message = "Nie udało się usunąć spotkania, spróbuj ponownie";});
     },
   }
 }
